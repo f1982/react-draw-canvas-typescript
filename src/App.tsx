fd917f923b738dcefe1c603f3eb639b8dd210174
Andy Cao <caojundan@gmail.com>
2022-09-11 06:41:22 +1200

    refactor: restructure the folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
-import React from 'react';
-import DrawCanvas from './comp/DrawCanvas';
-import { drawLife } from './DrawLife';
-import drawSomthing from './Drawing'
+import DrawCanvas from './components/DrawCanvas/DrawCanvas';
+import drawSomthing from './Drawing';
 function App() {
-
-
 
   return (
     <>
-      <DrawCanvas style={{ width: '100%', height: '100vh' }} draw={drawSomthing} />
-      <div style={{ position: 'absolute', top: 0, left: 0, bottom: 0, right: 0 }}>
-        <h1 style={{ color: '#ffffff', fontSize: '10rem', textAlign: 'center' }}>ANDY CAO</h1>
+      <DrawCanvas
+        style={{ width: '100%', height: '100vh' }}
+        draw={drawSomthing}
+      />
+      <div style={{ position: 'absolute', inset: '1rem' }}>
+        <h1 style={{ color: '#ffffff' }}>Canvas + React</h1>
       </div>
     </>
   );
--- src/Drawing.tsx
+++ src/Drawing.tsx
@@ -3,13 +3,19 @@ const drawSomthing = (canvas: HTMLCanvasElement, frameCount: number) => {
   const ctx = canvas.getContext('2d')!
   ctx.clearRect(0, 0, canvas.width, canvas.height)
 
-  ctx.fillStyle = "blue";
+  // draw background
+  ctx.fillStyle = "gray";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-
-  ctx.fillStyle = '#000000'
+  // draw a ball
+  ctx.fillStyle = 'yellow'
   ctx.beginPath()
-  ctx.arc(50, 100, 20 * Math.sin(frameCount * 0.05) ** 2, 0, 2 * Math.PI)
+  ctx.arc(
+    canvas.width / window.devicePixelRatio / 2,
+    canvas.height / window.devicePixelRatio / 2,
+    20 * Math.sin(frameCount * 0.05) ** 2 + 10,
+    0,
+    2 * Math.PI)
   ctx.fill()
 }
 
