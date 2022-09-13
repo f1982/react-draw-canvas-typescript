ea561be96762e6b807d1b8475e776ce646e70dd4
Andy Cao <caojundan@gmail.com>
2022-09-13 19:46:53 +1200

    chore: remove unused files

--- src/Test.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.sum = void 0;
-function sum(a, b) {
-    return a + b;
-}
-exports.sum = sum;
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
 import DrawCanvas from "./DrawCanvas";
 import useDrawCanvas from "./useDrawCanvas";
-import { sum } from './Test'
 
 export {
   DrawCanvas,
   useDrawCanvas,
-  sum
 }
